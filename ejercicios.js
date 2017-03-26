4fafe6e7146ce13d3ebee773a1011b1dc24dcdc4
andresfeliper3 <andresfeliper.2001@gmail.com>
2017-03-26 11:36:55 -0500

    Update ejercicios.js

--- ejercicios.js
+++ ejercicios.js
@@ -3,31 +3,31 @@
 // 1. Realiza una función que sume dos números
 function suma(a, b)
 {
-
+return a+b;
 }
 
 // 2. Realiza una función que reste dos números
 function resta(a,b)
 {
-
+return a-b
 }
 
 // 3. Realiza una función que multiplique dos números
 function multiplicacion(a,b)
 {
-
+return a*b
 }
 
 // 4. Realiza una función que divida dos números
 function division(a,b)
 {
-
+return a/b
 }
 
 // 5. Realiza una función que salude diciendo "Hola <nombre>"
 function hola(nombre)
 {
-
+document.write("Hola" + " " + nombre);
 }
 
 // 6. Realiza una función retorne el valor de verdad opuesto
@@ -39,5 +39,10 @@ function negacion(booleano)
 // 7. Realiza una función diga si dos valores son iguales
 function iguales(valor1, valor2)
 {
-
+if (valor1 === valor2) {
+  console.log("Los valores son iguales");
+} else {
+  console.log("Los valores no son iguales");
+   }
 }
+
